2b3e09662c15b34cb554e206882b3c89f7fd21a3
Archer Heffern <archerheffern@Archers-MacBook-Air.local>
2022-06-23 22:13:02 -0400

    worked with async await

--- promises/promise3.js
+++ promises/promise3.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 // instead of using .then and .catch, we can use async and await to clean the code up even more
 
 // --------------------------------------------------------------------------
-let asyncFunction1 = (url) => {
+let readFileAsync = (url) => {
     return new Promise((resolve, reject) => {
         fs.readFile(url, "utf-8", (err, result) => {
             if (err) {
@@ -14,29 +14,44 @@ let asyncFunction1 = (url) => {
         })
     })
 }
+
 // --------------------------------------------------------------------------
-const asyncFunction2 = (url) => {
+
+let writeFileAsync = (url, words) => {
     return new Promise((resolve, reject) => {
-        fs.readFile(url, "utf-8", (err, data) => {
+        fs.writeFile(url, words, { flag: "a" }, (err) => {
             if (err) { reject(err) }
-            else if (data) {
-                resolve(data)
-            };
+            else { resolve("sucessfully added content") }
         })
     })
 }
 
-const result = async () => {
-    try {
-        let one = await asyncFunction1("./texts/one.txt")
-        let two = await asyncFunction2("./texts/two.txt")
-        console.log(one);
-        console.log(two);
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
 // --------------------------------------------------------------------------
-result()
 
+// readFileAsync("./texts/one.txt")
+//     .then(result => {
+//         console.log(result)
+//         return result
+//     })
+//     .then(result => {
+//         writeFileAsync("./texts/junk.txt", result)
+//         return;
+//     })
+//     .then(() => readFileAsync("./texts/junk.txt"))
+//     .then(result => console.log(`\n${result}`))
+//     .catch(err => console.log(err))
+
+const results = async function () {
+    try {
+        let textOne = await readFileAsync("./texts/one.txt")
+        console.log(textOne)
+        await writeFileAsync("./texts/junk.txt", textOne)
+        let trashText = await readFileAsync("./texts/junk.txt")
+        console.log(trashText)
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+results()
